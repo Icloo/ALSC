0c6535abc309ccec09f84399025d3908cb1d2d44
samadi2 <satourdine@gmail.com>
2023-04-07 16:15:59 +0200

    ajouter

--- view/template/header.php
+++ view/template/header.php
@@ -32,7 +32,7 @@
         <a class="nav-link" href="./Contact.php">Contact</a>
         </li>
    
-      <button class="btn btn-outline-success" type="button"><a href="./Contribution.php">Dons & Adhérence</a></button>
+      <a href="./Contribution.php"><button class="btn btn-outline-success lien " type="button">Dons & Adhérence </button></a>
 
       <button class="btn btn-outline-success testtest" type="button" id="dark">Mode sombre <i class="fa-solid fa-moon"></i></button>
       
